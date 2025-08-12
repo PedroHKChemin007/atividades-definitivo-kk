89a3232a3a8df9a96d99f52e8f4f0251681c2257
Pedro h k chemin <pedrochemin1976@gmail.com>
2025-08-12 10:22:01 -0300

    atividadecorretaaa

--- QUESTOES/atv5b.js
+++ QUESTOES/atv5b.js
@@ -3,3 +3,32 @@ parâmetro para cada operação básica (adição, subtração, divisão e multi
 valor para onde a função for chamada. Ao final, executar cada operação 2 vezes com valores
 diferentes. */
 const prompt = require("prompt-sync")();
+console.log("digte os seus dois valores:");
+let numero1 = Number(prompt("--> "));
+let numero2 = Number(prompt("--> "));
+let resultsoma,resultsub,resultmul, resultdiv;
+let resutsoma = somar(numero1, numero2);
+let resutsub = subtrair(numero1, numero2);
+let resutmult = multiplicar(numero1, numero2);
+let resutdiv = dividir(numero1, numero2);
+function somar(num1, num2){
+    return resultsoma = num1 + num2;
+
+}
+function subtrair(num1, num2){
+    return resultsub =num1 - num2;
+}
+function multiplicar(num1, num2){
+    return resultmul = num1 * num2;
+}
+function dividir(num1, num2){
+    if (num1 > 0 || num2 > 0){
+        return resultdiv = num1 / num2;
+    } else {
+        return resultdiv = 0;
+    }
+}
+console.log(`Esse é o resultado da sua soma:${resultsoma}`);
+console.log(`Esse é o resultado da sua subtração:${resultsub}`);
+console.log(`Esse é o resultado da sua multiplicação:${resultmul}`);
+console.log(`Esse é o resultado da sua divisão:${resultdiv}`);
